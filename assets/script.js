38fafe55e59f3958890bfd5bb25eed97892972a2
ByYasin <pro.yasin@hotmail.com>
2023-07-19 21:51:47 +0300

    Added "Milio" champion.

--- assets/script.js
+++ assets/script.js
@@ -117,6 +117,7 @@ $(document).ready(function() {
       { name: 'Bel\'Veth', role: 'Fighter', image: 'https://lolg-cdn.porofessor.gg/img/d/champion-icons/13.12/64/200.png', lane_image: 'images/jungle.png' },
       { name: 'K\'Sante', role: 'Tank', image: 'https://lolg-cdn.porofessor.gg/img/d/champion-icons/13.12/64/897.png', lane_image: 'images/topLane.png' },
       { name: 'Vladimir', role: 'Mage', image: 'https://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/Vladimir.png', lane_image: 'images/midLane.png' },
+      { name: 'Milio', role: 'Mage', image: 'https://cdn.mobalytics.gg/assets/lol/images/dd/champions/icons/milio.png?V3', lane_image: 'images/support.png' },
       // other champions
     ];
 
